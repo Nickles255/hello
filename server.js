91177576608fb72abe81e7346befcbdbf3084bee
clin94118 <clin@ccsf.edu>
2024-10-17 18:04:59 -0700

    third attempt

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/hello'));
+app.use(express.static(${__dirname} + '/dist/hello'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/hello/browser/index.html'));
+res.sendFile(path.join('./dist/hello/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log('Server started on port ' + process.env.PORT || 8080);
